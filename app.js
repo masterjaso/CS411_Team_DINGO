254a457cbdd3cef7c7fe002a02906722d0951116
Jason Neal <masterjaso@gmail.com>
2018-02-08 02:07:05 +0000

    Initial Web App Load

---
+++ app.js
@@ -0,0 +1,56 @@
+//Include libraries for use
+var util = require('util');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mssql = require('mssql')
+
+//Routing targets - place route handling targets here
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+//Declare express application variable
+var app = express();
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Set application routing to destination pages
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//Start webserver listening on PORT/IP
+console.log('Starting server at:  ' + process.env.IP + ':' + process.env.PORT);
+app.listen(process.env.PORT, process.env.IP)
+
+module.exports = app;
