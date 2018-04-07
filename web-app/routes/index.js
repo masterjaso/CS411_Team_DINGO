00f1fd8b986f8382ac471702fe535db515687d93
Anchie <hanna@Anchies-iMac.local>
2018-04-07 16:09:06 -0600

    "more updates"

--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -4,9 +4,11 @@ const mysql = require('mysql2/promise');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let conn, k;
+  let conn, k, c, d;
   let occ = [];
   let hpaid = [];
+  let states = [];
+
   try{
     conn = await mysql.createConnection(req.dbOpt);
     
@@ -30,7 +32,23 @@ router.get('/', async function(req, res, next) {
     conn.end();
   }
   catch(e){console.log(e);}
-  res.render('index', { title: 'Career Explorer', occ : JSON.stringify(k), hpaid : JSON.stringify(c)});
+
+  try{
+    conn = await mysql.createConnection(req.dbOpt);
+    
+    //Get Occupation List
+    let [rows, fields] = await conn.execute("SELECT DISTINCT STATENAME, sum(TOT_EMP) as SUM from dingo.STATE S1, dingo.OCC_STATS OS WHERE " +
+                                " S1.STATE_ID = OS.STATE_ID AND OCC_TITLE <> 'All Occupations' group by YEAR_ID;", states);
+     
+    
+    d = rows;
+    conn.end();
+  }
+  
+  catch(e){console.log(e);}
+
+  console.log(d);
+  res.render('index', { title: 'Career Explorer', occ : JSON.stringify(k), hpaid : JSON.stringify(c), states : JSON.stringify(d)});
 });
 
 module.exports = router;
